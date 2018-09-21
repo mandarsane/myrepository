e6a4447040856e66b69725a86e3a05b110ee9ed6
mandarsane <43445772+mandarsane@users.noreply.github.com>
2018-09-21 16:26:45 +0530

    Update Document.java

--- Document.java
+++ Document.java
@@ -3,5 +3,6 @@ public static void main(String args[]){
 System.out.println("Hello World");
   System.out.println("This line was written by mandar");
   System.out.println("This line is written in branch testing code");
+  System.out.println("Direct master edit");
 }
 }
